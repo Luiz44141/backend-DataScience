c1e4b900638c7fd797410dac71b795bd395bbc7d
gustavokoichi <guga_koichi@hotmail.com>
2026-06-12 19:15:47 -0300

    feat: modelo adicionado, dependências resolvidas servidor on

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pickle
+import joblib
 import pandas as pd
 
-# Inicializa a aplicação FastAPI
 app = FastAPI(
     title="API - Predição de Saúde Mental (Adolescentes)",
-    description="API para classificar o impacto das redes sociais na saúde mental.",
+    description="API que verifica o nível de ansiedade e suas correlações na saúde mental de adolescentes.",
     version="1.0.0"
 )
 
-# Configuração de CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # PESSOAL DO FRONTEND, SUBSTITUAM O '*' PELO DOMÍNIO DO SEU FRONTEND (ex: 'http://localhost:3000')
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ==========================================
-# 1. DEFINIÇÃO DOS DADOS DE ENTRADA
-# ==========================================
-# Baseado nas colunas traduzidas no Google Colab
 class TeenHealthData(BaseModel):
     idade: int
     genero: str
@@ -35,33 +29,14 @@ class TeenHealthData(BaseModel):
     atividade_fisica: float
     nivel_interacao_social: str
     nivel_estresse: int
-    nivel_ansiedade: int
     nivel_vicio: int
 
-# ==========================================
-# 2. CARREGAMENTO DO MODELO (MOCK TEMPORÁRIO)
-# ==========================================
-# TODO: Quando o modelo real estiver pronto, descomente o bloco abaixo e apague a classe Mock.
-
-'''
 try:
-    with open("models/modelo_classificacao.pkl", "rb") as f:
-        modelo = pickle.load(f)
+    modelo = joblib.load("models/melhor_modelo.pkl")
+    print("Modelo carregado com sucesso!")
 except FileNotFoundError:
     modelo = None
     print("Aviso: Modelo não encontrado. Verifique o diretório 'models/'.")
-'''
-
-class MockModel:
-    def predict(self, df):
-        # Simula uma predição
-        return [1] 
-
-modelo = MockModel()
-
-# ==========================================
-# 3. ENDPOINTS DA API
-# ==========================================
 
 @app.get("/")
 def home():
@@ -69,18 +44,15 @@ def home():
 
 @app.post("/predict")
 def fazer_predicao(dados: TeenHealthData):
-    # 1. Transforma os dados recebidos do JSON em um Dicionário, e depois em um DataFrame do Pandas
     dados_dict = dados.model_dump()
     df_entrada = pd.DataFrame([dados_dict])
         
-    # 2. Realiza a predição
     try:
         resultado = modelo.predict(df_entrada)
         
-        # 3. Retorna o resultado para o frontend
         return {
             "status": "sucesso",
-            "predicao_indicador_depressao": int(resultado[0]),
+            "predicao_ansiedade_alta": int(resultado[0]),
             "dados_recebidos": dados_dict
         }
     except Exception as e:
